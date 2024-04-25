a3b91ad661c9f852841025d1eb59be5d370fcbb1
Fernando Cardoso <fc54413@alunos.fc.ul.pt>
2024-04-25 13:02:06 +0100

    fix alguns problemas

--- controllers/websiteController.js
+++ controllers/websiteController.js
@@ -46,52 +46,65 @@ exports.website_create_post = asyncHandler(async (req, res, next) => {
   });
 
 
-// Controlador para atualizar um website existente
-exports.website_update = asyncHandler(async (req, res) => {
-  // Extrai o ID do website do corpo da solicitação
-  const websiteId = req.body._id;
-
-  // Verifica se o website com o ID fornecido existe no banco de dados
-  const website = await Website.findById(websiteId);
-  console.log(req.body);
-  if (!website) {
-    return res.status(404).json({ error: "Website not found" });
-  }
-
-
-  // Atualiza os campos do website com base nos dados fornecidos no corpo da solicitação
-  website.url = req.body.url._id; // Use apenas o ObjectId do objeto Url
-  console.log(website);
-
-  if (!website.urls) {
-    website.urls = []; // Inicializa a lista de URLs se ainda não existir
-  }
-  // Verifica se há novos elementos no array 'urls'
-  if (req.body.urls && req.body.urls.length > 0) {
-    // Itera sobre os novos elementos do array 'urls'
-    for (const urlData of req.body.urls) {
-      // Cria um novo objeto Url
-      const newUrl = new Url({
-        link: urlData.link,
-        estado: urlData.estado,
-        ultima_aval: urlData.ultima_aval
-      });
-   
-
-      await newUrl.save();
-
-      website.urls.push(newUrl._id);
+  exports.website_update = asyncHandler(async (req, res) => {
+    // Extrai o ID do website do corpo da solicitação
+    const websiteId = req.body._id;
+  
+    // Verifica se o website com o ID fornecido existe no banco de dados
+    const website = await Website.findById(websiteId);
+    console.log(req.body);
+    if (!website) {
+      return res.status(404).json({ error: "Website not found" });
     }
-  }
-
-
-  // Salva as alterações no banco de dados
-  await website.save();
-  console.log(website);
-  // Responde com o website atualizado
-  res.json(website);
-});
-
+  
+    // Atualiza os campos do website com base nos dados fornecidos no corpo da solicitação
+    website.url = req.body.url._id; // Use apenas o ObjectId do objeto Url
+  
+    if (!website.urls) {
+      website.urls = []; // Inicializa a lista de URLs se ainda não existir
+    }
+  
+    // Verifica se há novos elementos no array 'urls'
+    if (req.body.urls && req.body.urls.length > 0) {
+      // Itera sobre os novos elementos do array 'urls'
+      for (const urlData of req.body.urls) {
+        // Verifica se o URL já existe no banco de dados
+        let existingUrl = await Url.findOne({ link: urlData.link });
+  
+        if (existingUrl) {
+          // Se o URL já existe, atualiza os dados
+          existingUrl.estado = urlData.estado;
+          existingUrl.ultima_aval = urlData.ultima_aval;
+          await existingUrl.save();
+  
+          // Verifica se o ID do URL já está na lista de URLs do website
+          if (!website.urls.includes(existingUrl._id)) {
+            // Adiciona o ID do URL existente à lista de URLs do website apenas se ainda não estiver lá
+            website.urls.push(existingUrl._id);
+          }
+        } else {
+          // Se o URL não existe, cria um novo URL
+          const newUrl = new Url({
+            link: urlData.link,
+            estado: urlData.estado,
+            ultima_aval: urlData.ultima_aval
+          });
+  
+          await newUrl.save();
+  
+          // Adiciona o ID do novo URL à lista de URLs do website
+          website.urls.push(newUrl._id);
+        }
+      }
+    }
+  
+    // Salva as alterações no banco de dados
+    await website.save();
+    console.log(website);
+  
+    // Responde com o website atualizado
+    res.json(website);
+  });
 
 exports.website_detail = asyncHandler(async (req, res, next) => {
 
